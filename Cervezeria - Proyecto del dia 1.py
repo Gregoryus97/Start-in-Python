fde9e8b509c53a36801cde501b0449de98f0f358
Gregoryus97 <103522161+Gregoryus97@users.noreply.github.com>
2022-04-27 02:40:14 +0200

    Delete Cervezeria - Proyecto del dia 1.py

--- Cervezeria - Proyecto del dia 1.py
+++
@@ -1,2 +0,0 @@
-print("El nombre de tu cervera es: \n '" + input("Que ciudad quieres visitar mañana: ") + " " + input("tu color favorito: ") + "'")
-print("Muchas Felicidades, tu resultado ha sido exitoso")
