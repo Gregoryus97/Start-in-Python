5b5e8df0fbbd9d1c7cb05d0ce672cb58352736de
Gregoryus97 <103522161+Gregoryus97@users.noreply.github.com>
2022-04-27 02:40:27 +0200

    Add files via upload

---
+++ Cervezeria - Proyecto del dia 1.py
@@ -0,0 +1,4 @@
+print("¡Creador de nombres para tu primera cerveza!")
+input("Teclea cualquier letra para empezar ")
+print("El nombre de tu cerveza es: \n '" + input("Que ciudad quieres visitar mañana: ") + " " + input("tu color favorito: ") + "'")
+print("¡Muchas Felicidades!, tu resultado ha sido exitoso.")
