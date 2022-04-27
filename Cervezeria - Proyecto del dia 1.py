8c7424329b0dc446b8ef50d2e8ae05310208d6e4
Gregoryus97 <103522161+Gregoryus97@users.noreply.github.com>
2022-04-27 02:19:06 +0200

    Add files via upload

---
+++ Cervezeria - Proyecto del dia 1.py
@@ -0,0 +1,2 @@
+print("El nombre de tu cervera es: \n '" + input("Que ciudad quieres visitar mañana: ") + " " + input("tu color favorito: ") + "'")
+print("Muchas Felicidades, tu resultado ha sido exitoso")
